bc8576edbb376bc4869c0cb29784cc6e9ff4af51
Eric Gomez <65141753+realericvega@users.noreply.github.com>
2021-10-09 19:44:07 -0400

    Update SiloBot.kt

--- src/main/kotlin/SiloBot.kt
+++ src/main/kotlin/SiloBot.kt
@@ -1,6 +1,14 @@
 import dev.kord.core.Kord
+import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.on
 
 suspend fun main() {
     val client = Kord("ODk2NTI2NzQ2NjIxNDU2NDE0.YWIZmQ.hvq3Aa1JjUBLNUV2fU5O6RR3-1g")
+
+    client.on<MessageCreateEvent> {
+        if (message.content == "Test")
+            message.getChannel().createMessage("It works!")
+    }
+
     client.login()
 }
