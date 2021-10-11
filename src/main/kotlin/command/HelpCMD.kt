7e7b1e0b59636ac868b27645559d75a2265ca08d
Eric Gomez <65141753+realericvega@users.noreply.github.com>
2021-10-11 00:12:19 -0400

    Update HelpCMD.kt

--- src/main/kotlin/command/HelpCMD.kt
+++ src/main/kotlin/command/HelpCMD.kt
@@ -28,7 +28,7 @@ class HelpCMD : ICommand {
                             field {
                                 name = "General Commands"
                                 inline = true
-                                for (i in 7 downTo 0) {
+                                for (i in 6 downTo 0) {
                                     value = BotUtil.getCommandsMap()["general"]?.get(i).toString()
                                 }
                             }
@@ -37,35 +37,6 @@ class HelpCMD : ICommand {
                                 this.text = "Indeedious#0001"
                                 this.icon = member?.avatar?.cdnUrl?.toUrl()
                             }
-                        }
-                    }
-                } else {
-                    message.getChannel().createMessage {
-
-                        embed {
-                            description = "All Bot Commands"
-                            color = Color(eColor[0], eColor[1], eColor[2])
-                            field {
-                                inline = true
-                                name = "General Commands"
-                                for (command in BotUtil.getCommandsMap()["general"]!!) {
-                                    value = command
-                                }
-                            }
-
-                            field {
-                                inline = true
-                                name = "Admin Commands"
-                                for (command in BotUtil.getCommandsMap()["admin"]!!) {
-                                    value = command
-                                }
-                            }
-
-                            footer {
-                                this.text = "Indeedious#0001"
-                                this.icon = member?.avatar?.cdnUrl?.toUrl()
-                            }
-
                         }
                     }
                 }
