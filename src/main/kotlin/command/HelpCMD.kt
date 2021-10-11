144e5183116737876cdd10c2248c9633bd22c488
Eric Gomez <65141753+realericvega@users.noreply.github.com>
2021-10-11 17:04:55 -0400

    Update HelpCMD.kt

--- src/main/kotlin/command/HelpCMD.kt
+++ src/main/kotlin/command/HelpCMD.kt
@@ -7,6 +7,7 @@ import dev.kord.core.behavior.channel.createMessage
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.on
 import dev.kord.rest.builder.message.create.embed
+import kotlinx.datetime.Clock
 
 class HelpCMD : ICommand {
 
@@ -18,28 +19,42 @@ class HelpCMD : ICommand {
             val prefix = BotUtil.getCommandPrefix()
 
             if (args[0] == "${prefix}help") {
-                if (message.getAuthorAsMember()?.getPermissions()?.contains(Permission.Administrator) == true) {
-                    message.getChannel().createMessage {
+                message.getChannel().createMessage {
 
-                        embed {
-                            title = "My Commands"
-                            color = Color(eColor[0], eColor[1], eColor[2])
+                    embed {
+                        title = "General Commands"
+                        color = Color(eColor[0], eColor[1], eColor[2])
+                        timestamp = Clock.System.now()
 
-                            val builder: StringBuilder = StringBuilder()
+                        var builder: StringBuilder = StringBuilder()
 
-                            for (i in 0 until BotUtil.getCommandsMap()["general"]?.size!!) {
+                        if (message.getAuthorAsMember()?.getPermissions()?.contains(Permission.Administrator) == false) {
+                            for (i in 0 until BotUtil.getCommandsMap()["general"]?.size!!)
                                 builder.append("${BotUtil.getCommandsMap()["general"]!![i]}\n")
-                            }
+                        } else {
 
-                            description = builder.toString()
+//                            for (i in 0 until BotUtil.getCommandsMap()["general"]?.size!!)
+//                                builder.append("${BotUtil.getCommandsMap()["general"]!![i]}\n")
 
-                            footer {
-                                this.text = "Indeedious#0001"
-                                this.icon = member?.avatar?.cdnUrl?.toUrl()
+                            for (i in 0 until BotUtil.getCommandsMap()["admin"]?.size!!)
+                                builder = builder.append("${BotUtil.getCommandsMap()["admin"]!![i]}\n")
+
+                            field {
+                                name = "Admin Commands"
+                                inline = false
+                                value = builder.toString()
                             }
+                        }
+
+                        description = builder.toString()
+
+                        footer {
+                            this.text = "Indeedious#0001"
+                            this.icon = member?.avatar?.cdnUrl?.toUrl()
                         }
                     }
                 }
+
             }
         }
     }
